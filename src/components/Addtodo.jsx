24c84ac4412117734a8d63077466ddf936a002a9
Al Mahmud Alif <myself.amalif@gmail.com>
2024-09-22 23:09:46 +0600

    performance optimization

--- src/components/Addtodo.jsx
+++ src/components/Addtodo.jsx
@@ -1,32 +1,23 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 export default function Addtodo({ onNewItem }) {
-  const [todoname, setTodoName] = useState("");
-  const [dueDate, setDueDate] = useState("");
-  const handleNameChange = (e) => {
-    setTodoName(e.target.value);
-  };
-  const handleDateChange = (e) => {
-    setDueDate(e.target.value);
-  };
+  const todoName = useRef("");
+  const todoDate = useRef("");
+
   const handleAddButtonClick = () => {
-    onNewItem(todoname, dueDate);
-    setTodoName("");
-    setDueDate("");
+    onNewItem(todoName.current.value, todoDate.current.value);
+    todoName.current.value = "";
+    todoDate.current.value = "";
   };
+
   return (
     <div className="container text-center">
       <div className="row">
         <div className="col-6">
-          <input
-            onChange={handleNameChange}
-            type="text"
-            placeholder="Enter todo here"
-            value={todoname}
-          />
+          <input ref={todoName} type="text" placeholder="Enter todo here" />
         </div>
         <div className="col-4">
-          <input onChange={handleDateChange} type="date" value={dueDate} />
+          <input type="date" ref={todoDate} />
         </div>
         <div className="col-2">
           <button className="btn btn-success" onClick={handleAddButtonClick}>
